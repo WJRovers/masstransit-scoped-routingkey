ca57ce08f58361dbec3c627526b9cb09c5e59913
W.J.Rovers <w.j.rovers@tools4ever.com>
2021-10-27 14:20:49 +0200

    Decoration happens via filter

--- MT.DI.Test/Consumers/StartPingConsumer.cs
+++ MT.DI.Test/Consumers/StartPingConsumer.cs
@@ -1,20 +1,25 @@
 ﻿using System.Threading.Tasks;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using MT.DI.Test.Messages;
 
 namespace MT.DI.Test.Consumers
 {
     public class StartPingConsumer : IConsumer<StartPingRequest>
     {
+        private readonly ILogger<StartPingRequest> _logger;
+
+        public StartPingConsumer(ILogger<StartPingRequest> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task Consume(ConsumeContext<StartPingRequest> context)
         {
             var fooValue = context.Headers.Get<string>("foo");
-            await context.Publish<PingRequest>(new
-                {
-                    Message = context.Message.Message
-                }, ctx =>
-                    ctx.Headers.Set("foo", fooValue)
-            );
+            
+            _logger.LogInformation("Starting a ping request while the header current has a value of {FoodHeaderValue}", fooValue);
+            await context.Publish<PingRequest>(new {Message = context.Message.Message});
         }
     }
 }
